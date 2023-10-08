709f74147190c0274bb02a2b6aa3a1c631797af0
haowong <haowong.seu@gmail.com>
2023-10-08 16:48:17 +0800

    update

---
+++ 开发内容/第六周/未提交/修剪裁切/CutBaseOnFea.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SMGI.Common;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Controls;
+using System.Windows.Forms;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Display;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.esriSystem;
+
+namespace SMGI.Plugin.CollaborativeWorkWithAccount
+{
+    public class CutBaseOnFea : SMGITool
+    {
+        IEngineEditor editor;
+        /// <summary>
+        /// 线型符号
+        /// </summary>
+        ISimpleLineSymbol lineSymbol;
+        /// <summary>
+        /// 线型反馈
+        /// </summary>
+        INewLineFeedback lineFeedback;
+        IFeature basefea = null;//基线裁切要素
+        IFeatureClass CheckClass;//当前编辑图层
+        public CutBaseOnFea()
+        {
+            m_caption = "修剪裁切";
+            NeedSnap = false;
+        }
+        public override void OnClick()
+        {
+            editor = m_Application.EngineEditor;
+
+            IEnumFeature mapEnumFeature = m_Application.MapControl.Map.FeatureSelection as IEnumFeature;
+            mapEnumFeature.Reset();
+            basefea = mapEnumFeature.Next();
+            IEngineEditSketch editsketch = m_Application.EngineEditor as IEngineEditSketch;
+            IEngineEditLayers engineEditLayer = editsketch as IEngineEditLayers;
+            IFeatureLayer featureLayer = engineEditLayer.TargetLayer as IFeatureLayer;
+            CheckClass = featureLayer.FeatureClass;
+            //#region Create a symbol to use for feedback
+            lineSymbol = new SimpleLineSymbolClass();
+            IRgbColor color = new RgbColorClass();	 //red
+            color.Red = 255;
+            color.Green = 0;
+            color.Blue = 0;
+            lineSymbol.Color = color;
+            lineSymbol.Style = esriSimpleLineStyle.esriSLSSolid;
+            lineSymbol.Width = 1.5;
+            (lineSymbol as ISymbol).ROP2 = esriRasterOpCode.esriROPNotXOrPen;//这个属性很重要
+            //#endregion
+            lineFeedback = null;
+            //用于解决在绘制feedback过程中进行地图平移出现线条混乱的问题
+            m_Application.MapControl.OnAfterScreenDraw += new IMapControlEvents2_Ax_OnAfterScreenDrawEventHandler(MapControl_OnAfterScreenDraw);
+        }
+        public override void OnMouseDown(int button, int shift, int x, int y)
+        {
+            if (button != 1)
+            {
+                return;
+            }
+            if (lineFeedback == null)
+            {
+                var dis = m_Application.ActiveView.ScreenDisplay;
+                lineFeedback = new NewLineFeedbackClass { Display = dis, Symbol = lineSymbol as ISymbol };
+                lineFeedback.Start(ToSnapedMapPoint(x, y));
+            }
+            else
+            {
+                lineFeedback.AddPoint(ToSnapedMapPoint(x, y));
+            }
+        }
+        public override void OnMouseMove(int button, int shift, int x, int y)
+        {
+            if (lineFeedback != null)
+            {
+                lineFeedback.MoveTo(ToSnapedMapPoint(x, y));
+            }
+        }
+        private void MapControl_OnAfterScreenDraw(object sender, IMapControlEvents2_OnAfterScreenDrawEvent e)
+        {
+            if (lineFeedback != null)
+            {
+                lineFeedback.Refresh(m_Application.ActiveView.ScreenDisplay.hDC);
+            }
+        }
+        public override bool Deactivate()
+        {
+            //卸掉该事件
+            m_Application.MapControl.OnAfterScreenDraw -= new IMapControlEvents2_Ax_OnAfterScreenDrawEventHandler(MapControl_OnAfterScreenDraw);
+            return base.Deactivate();
+        }
+        public override void OnDblClick()
+        {
+            List<IFeature> fea = new List<IFeature>();
+            List<IFeature> feacopy = new List<IFeature>();
+            List<IFeature> feadlet = new List<IFeature>();
+            IPolyline polyline = lineFeedback.Stop();
+            lineFeedback = null;
+            //双击完毕进行线条的打断
+            if (polyline.IsEmpty)
+                return;
+            editor.StartOperation();
+            #region 收集与划线相交的要素
+            ISpatialFilter filter = new SpatialFilterClass();
+            filter.SpatialRel = esriSpatialRelEnum.esriSpatialRelIntersects;
+            filter.Geometry = polyline;
+            IFeature inFeature = null;
+            IFeatureCursor maskedCursor = CheckClass.Search(filter, false);
+            while ((inFeature = maskedCursor.NextFeature()) != null)
+            {
+                fea.Add(inFeature);
+            }
+
+            foreach (var feature in fea)
+            {
+                // 创建新要素并设置形状
+                IFeature newFeature = CheckClass.CreateFeature(); // 这里根据你的要素类类型创建新要素
+                newFeature.Shape = feature.Shape;
+
+                // 将新要素添加到新列表中
+                feacopy.Add(newFeature);
+            }
+
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(maskedCursor);
+            #endregion
+            try
+            {
+                #region 执行线打断
+                for (int i = 0; i < feacopy.Count; i++)
+                {
+                    IFeature tempfea = feacopy[i];
+                    IPolyline ply = tempfea.ShapeCopy as IPolyline;
+                    ITopologicalOperator2 pTopo = (ITopologicalOperator2)ply;
+                    IGeometry InterGeo = pTopo.Intersect(basefea.Shape, esriGeometryDimension.esriGeometry0Dimension);
+                    IGeometryCollection geoCol = InterGeo as IGeometryCollection;
+                    if (geoCol.GeometryCount == 0) { continue; }
+                    List<int> featurelist = new List<int>();
+                    //    List<int> splitfeaturelist = new List<int>();
+                    for (int j = 0; j < geoCol.GeometryCount; j++)
+                    {
+                        IPoint splitPt = new PointClass();
+                        splitPt = geoCol.get_Geometry(j) as IPoint;
+                        IFeatureEdit pFeatureEdit = null;
+                        if (featurelist.Count != 0)
+                        {
+                            for (int k = 0; k < featurelist.Count; k++)
+                            {
+                                IFeature featemp = CheckClass.GetFeature(featurelist[k]);
+                                //   if (splitfeaturelist.Contains(featurelist[j])) { continue; }//已经分割过一次的跳过
+                                IPolyline polylinetemp = featemp.Shape as IPolyline;
+                                IRelationalOperator relationalOperator = polylinetemp as IRelationalOperator;//好接口
+                                if (relationalOperator.Contains(splitPt))
+                                {
+                                    if ((featemp.ShapeCopy as IPolycurve).Length == 0) { continue; }
+                                    pFeatureEdit = (IFeatureEdit)featemp;
+                                    featurelist.Remove(featurelist[j]);
+                                    //    splitfeaturelist.Add(featemp.OID);
+                                    break;
+                                }
+
+                            }
+                        }
+                        else
+                        {
+                            if ((tempfea.ShapeCopy as IPolycurve).Length == 0) { continue; }
+                            pFeatureEdit = (IFeatureEdit)tempfea; //splitfeaturelist.Add(pFeature.OID); 
+
+                        } ISet pFeatureSet = pFeatureEdit.Split(splitPt);
+                        if (pFeatureSet != null)
+                        {
+                            pFeatureSet.Reset();
+                            IFeature feature = null;
+                            while ((feature = pFeatureSet.Next() as IFeature) != null)
+                            {
+                                featurelist.Add(feature.OID);
+                                IRelationalOperator relationalOperatorPt = polyline as IRelationalOperator;//好接口
+                                if (!relationalOperatorPt.Disjoint(feature.Shape))//相交
+                                {
+                                    feadlet.Add(feature);
+                                }
+                                else
+                                {
+                                    feadlet.Add(feature);
+                                    fea[i].Shape = feature.Shape;
+                                    fea[i].Store();
+                                    int idxSTACOD = -1;
+                                    int idxVERS = -1;
+                                    idxSTACOD = fea[i].Fields.FindField(ServerDataInitializeCommand.CollabSTACOD);
+                                    fea[i].set_Value(idxSTACOD, "修改");
+                                    idxVERS = fea[i].Fields.FindField(ServerDataInitializeCommand.CollabVERSION);
+                                    fea[i].set_Value(idxVERS, cmdUpdateRecord.EditState);
+                                }
+                            }
+                        }
+                    }
+
+                }
+                #endregion
+                foreach (var item in feadlet)
+                {
+                    try
+                    {
+                        item.Delete();
+                    }
+                    catch (Exception ex) { continue; }
+                }
+            }
+            catch (Exception ex) { }
+            m_Application.EngineEditor.StopOperation("修剪裁切");
+            m_Application.MapControl.Refresh();
+        }
+        public override bool Enabled
+        {
+            get
+            {
+                if (m_Application != null && m_Application.Workspace != null && m_Application.EngineEditor.EditState == esriEngineEditState.esriEngineStateEditing)
+                {
+                    if (m_Application.MapControl.Map.SelectionCount != 1)
+                        return false;
+                    IEnumFeature mapEnumFeature = m_Application.MapControl.Map.FeatureSelection as IEnumFeature;
+                    mapEnumFeature.Reset();
+                    IFeature feature = mapEnumFeature.Next();
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(mapEnumFeature);
+                    if (feature != null)
+                    {
+                        if (feature.Shape is IPolyline)
+                        {
+                            System.Runtime.InteropServices.Marshal.ReleaseComObject(feature);
+                            return true;
+                        }
+                        else
+                        {
+                            System.Runtime.InteropServices.Marshal.ReleaseComObject(feature);
+                            return false;
+                        }
+                    }
+                    else
+                        return false;
+                }
+                else
+                    return false;
+            }
+        }
+    }
+}
---
+++ 开发内容/第六周/未提交/线打断/LineBreakTool.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SMGI.Common;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Controls;
+using System.Windows.Forms;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Display;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.esriSystem;
+
+namespace SMGI.Plugin.CollaborativeWorkWithAccount
+{
+    public class LineBreakTool : SMGITool
+    {
+        private IFeature _breakFeature;
+
+        public LineBreakTool()
+        {
+            m_caption = "线打断";
+            m_cursor = new System.Windows.Forms.Cursor(GetType().Assembly.GetManifestResourceStream(GetType(), "修线.cur"));
+            NeedSnap = false;
+            _breakFeature = null;
+        }
+
+        public override bool Enabled
+        {
+            get
+            {
+                return m_Application != null && m_Application.Workspace != null &&
+                       m_Application.EngineEditor.EditState == ESRI.ArcGIS.Controls.esriEngineEditState.esriEngineStateEditing;
+            }
+        }
+
+        private ILayer layer;
+        public override void OnClick()
+        {
+            var map = m_Application.ActiveView.FocusMap;
+            var selection = map.FeatureSelection;
+            if (map.SelectionCount != 1)
+            {
+                MessageBox.Show("请选中单条可编辑的线要素!");
+                return;
+            }
+
+
+            IEnumFeature selectEnumFeature = (selection as MapSelection) as IEnumFeature;
+            selectEnumFeature.Reset();
+            IFeature fe = selectEnumFeature.Next();
+
+            if (fe.Shape.GeometryType != esriGeometryType.esriGeometryPolyline)
+            {
+                MessageBox.Show("请选中一条线要素");
+                return;
+            }
+
+            string fn = fe.Class.AliasName.ToUpper();
+            layer = m_Application.Workspace.LayerManager.GetLayer(l => (l is IGeoFeatureLayer) && ((l as IGeoFeatureLayer).FeatureClass == fe.Class)).FirstOrDefault();
+            if (!(m_Application.EngineEditor as IEngineEditLayers).IsEditable(layer as IFeatureLayer))
+            {
+                MessageBox.Show("请选中一条可编辑的线要素");
+                return;
+            }
+
+            NeedSnap = true;
+            _breakFeature = fe;
+        }
+
+        public override void OnMouseDown(int button, int shift, int x, int y)
+        {
+            if (button != 1)
+                return;
+
+            if (_breakFeature == null)
+                return;
+
+            //获取当前点
+            IPoint currentMouseCoords = m_Application.ActiveView.ScreenDisplay.DisplayTransformation.ToMapPoint(x, y);
+            ISnappingResult snapResult = m_snapper.Snap(currentMouseCoords);
+            if (snapResult == null)
+            {
+                MessageBox.Show("请将打断点置于捕捉容差范围内！");
+                return;
+            }
+            currentMouseCoords = snapResult.Location;
+
+            m_Application.EngineEditor.StartOperation();
+            try
+            {
+                string StaCod = _breakFeature.get_Value(_breakFeature.Fields.FindField(ServerDataInitializeCommand.CollabSTACOD)).ToString();
+
+                bool bCoola = true;
+                #region 协同
+                if (_breakFeature.Fields.FindField(ServerDataInitializeCommand.CollabGUID) == -1)
+                {
+                    bCoola = false;
+                }
+
+                string smgiGUID = "";
+                if (bCoola)
+                {
+                    smgiGUID = _breakFeature.get_Value(_breakFeature.Fields.FindField(ServerDataInitializeCommand.CollabGUID)).ToString();
+                }
+
+                int smgiver = 0;
+                if (bCoola)
+                {
+                    string STACOD = _breakFeature.get_Value(_breakFeature.Fields.FindField(ServerDataInitializeCommand.CollabSTACOD)).ToString();
+                    MessageBox.Show("STACOD: " + STACOD.ToString());
+                    if (STACOD == "删除")
+                    {
+                        MessageBox.Show("该要素为删除要素，不支持该操作！");
+                        return;
+                    }
+                }
+
+                if (bCoola)
+                {
+                    _breakFeature.set_Value(_breakFeature.Fields.FindField(ServerDataInitializeCommand.CollabVERSION), cmdUpdateRecord.NewState);//201605，直接删除的标志
+                }
+                #endregion
+
+                ISet pFeatureSet = (_breakFeature as IFeatureEdit).Split(currentMouseCoords);
+                if (pFeatureSet != null)//201605
+                {
+                    pFeatureSet.Reset();
+
+                    List<IFeature> flist = new List<IFeature>();
+                    int maxIndex = -1;
+                    double maxLen = 0;
+                    while (true)
+                    {
+                        IFeature fe = pFeatureSet.Next() as IFeature;
+                        if (fe == null)
+                        {
+                            break;
+                        }
+
+                        if ((fe.Shape as IPolyline).Length > maxLen)
+                        {
+                            maxLen = (fe.Shape as IPolyline).Length;
+                            maxIndex = flist.Count();
+                        }
+                        flist.Add(fe);
+
+                    }
+
+                    #region 协同，长的一段保持原GUID
+                    if (cmdUpdateRecord.EnableUpdate)
+                    {
+                        for (int k = 0; k < flist.Count(); ++k)
+                        {
+                            if (maxIndex == k)
+                            {
+                                m_Application.ActiveView.FocusMap.SelectFeature(layer, flist[k]);
+                                if (StaCod == "原始" || StaCod == "修改")
+                                {
+                                    flist[k].set_Value(flist[k].Fields.FindField(ServerDataInitializeCommand.CollabSTACOD), "修改");//201605
+                                }
+
+                                if (smgiver >= 0 || smgiver == cmdUpdateRecord.EditState)//服务器中下载下来的要素，在打断等操作时，最大部分的协同版本号应该修改（这样才能更新服务器）2017.06.27
+                                {
+                                    if (bCoola)
+                                    {
+                                        flist[k].set_Value(flist[k].Fields.FindField(ServerDataInitializeCommand.CollabVERSION), cmdUpdateRecord.EditState);
+                                    }
+                                }
+
+                                if (bCoola)
+                                {
+                                    flist[k].set_Value(flist[k].Fields.FindField(ServerDataInitializeCommand.CollabGUID), smgiGUID);//默认由最大的新要素继承原要素的smgiGUID
+                                }
+
+                                flist[k].Store();
+                                
+                                break;
+                            }
+                        }
+                    }
+                    #endregion
+                }
+
+                m_Application.EngineEditor.StopOperation("线打断");
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.WriteLine(ex.Message);
+                System.Diagnostics.Trace.WriteLine(ex.Source);
+                System.Diagnostics.Trace.WriteLine(ex.StackTrace);
+
+                MessageBox.Show(ex.Message);
+
+                m_Application.EngineEditor.AbortOperation();
+            }
+
+            NeedSnap = false;
+            _breakFeature = null;
+            m_Application.MapControl.Refresh();
+
+        }
+
+    }
+}
