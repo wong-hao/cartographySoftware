51278307e230afcbcf0f691bbe86b4f6dbaba2c7
haowong <haowong.seu@gmail.com>
2024-01-04 10:49:12 +0800

    update

---
+++ SMGI.Plugin.CollaborativeWorkWithAccount/开发内容/第十六周/未提交/要素分层硬编码逻辑检查/CheckFCFieldValueFrm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SMGI.Common;
+
+namespace SMGI.Plugin.CollaborativeWorkWithAccount
+{
+    public partial class CheckFCFieldValueFrm : Form
+    {
+
+        public String LyrName;
+        private GApplication _app;
+        public string mdbPath;
+
+
+        public CheckFCFieldValueFrm(GApplication app)
+        {
+            InitializeComponent();
+            _app = app;
+        }
+
+        private void btOK_Click(object sender, EventArgs e)
+        {
+            DialogResult = System.Windows.Forms.DialogResult.OK;
+        }
+
+        private void roadLyrNameComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LyrName = roadLyrNameComboBox.SelectedItem.ToString();
+        }
+
+        private void CheckFCFieldValueFrm_Load(object sender, EventArgs e)
+        {
+            // 在构造函数或 Load 事件中添加选项
+            mdbPath = _app.Template.Root + "\\质检\\要素分层硬编码逻辑检查.mdb";
+            List<string> tableNames = GetTableNamesFromMDB(mdbPath);
+
+            // 使用 foreach 循环遍历列表中的元素
+            foreach (string tableName in tableNames)
+            {
+                roadLyrNameComboBox.Items.Add(tableName);
+            }
+
+            LyrName = String.Empty;
+
+            // 选择默认项（可选）
+            roadLyrNameComboBox.SelectedIndex = 0; // 默认选中第一个选项，如果需要选择第二个选项，将索引设置为 1
+        }
+
+        public static List<string> GetTableNamesFromMDB(string mdbFilePath)
+        {
+            List<string> tableNames = new List<string>();
+
+            string connString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + mdbFilePath + ";";
+            using (OleDbConnection conn = new OleDbConnection(connString))
+            {
+                conn.Open();
+
+                // 获取表格信息
+                var schema = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+
+                if (schema != null)
+                {
+                    foreach (DataRow row in schema.Rows)
+                    {
+                        string tableName = row["TABLE_NAME"].ToString();
+                        tableNames.Add(tableName);
+                    }
+                }
+            }
+
+            return tableNames;
+        }
+    }
+}
---
+++ SMGI.Plugin.CollaborativeWorkWithAccount/开发内容/第十六周/未提交/要素分层硬编码逻辑检查/CheckFCFieldValueFrm.designer.cs
@@ -0,0 +1,128 @@
+﻿namespace SMGI.Plugin.CollaborativeWorkWithAccount
+{
+    partial class CheckFCFieldValueFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btOK = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.roadLyrNameComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btOK);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.btCancel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 43);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(4);
+            this.panel1.Size = new System.Drawing.Size(302, 31);
+            this.panel1.TabIndex = 9;
+            // 
+            // btOK
+            // 
+            this.btOK.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btOK.Location = new System.Drawing.Point(161, 4);
+            this.btOK.Name = "btOK";
+            this.btOK.Size = new System.Drawing.Size(64, 23);
+            this.btOK.TabIndex = 7;
+            this.btOK.Text = "确定";
+            this.btOK.UseVisualStyleBackColor = true;
+            this.btOK.Click += new System.EventHandler(this.btOK_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(225, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(9, 23);
+            this.panel2.TabIndex = 6;
+            // 
+            // btCancel
+            // 
+            this.btCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btCancel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btCancel.Location = new System.Drawing.Point(234, 4);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(64, 23);
+            this.btCancel.TabIndex = 5;
+            this.btCancel.Text = "取消";
+            this.btCancel.UseVisualStyleBackColor = true;
+            // 
+            // roadLyrNameComboBox
+            // 
+            this.roadLyrNameComboBox.FormattingEnabled = true;
+            this.roadLyrNameComboBox.Location = new System.Drawing.Point(113, 12);
+            this.roadLyrNameComboBox.Name = "roadLyrNameComboBox";
+            this.roadLyrNameComboBox.Size = new System.Drawing.Size(121, 20);
+            this.roadLyrNameComboBox.TabIndex = 11;
+            this.roadLyrNameComboBox.SelectedIndexChanged += new System.EventHandler(this.roadLyrNameComboBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "选择图层";
+            // 
+            // CheckFCFieldValueFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 74);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.roadLyrNameComboBox);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CheckFCFieldValueFrm";
+            this.Text = "要素分层硬编码逻辑检查";
+            this.Load += new System.EventHandler(this.CheckFCFieldValueFrm_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btOK;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.ComboBox roadLyrNameComboBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
