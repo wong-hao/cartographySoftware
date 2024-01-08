d2c9b56a7252993bfcf2d5f994b4863c12310972
haowong <haowong.seu@gmail.com>
2024-01-08 20:29:29 +0800

    update

---
+++ SMGI.Plugin.CollaborativeWorkWithAccount/开发内容/第十七周/要素分层硬编码逻辑检查/CheckFCFieldValueFrm.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+using SMGI.Common;
+
+namespace SMGI.Plugin.CollaborativeWorkWithAccount
+{
+    public partial class CheckFCFieldValueFrm : Form
+    {
+        public List<IFeatureLayer> CheckFeatureLayerList
+        {
+            get
+            {
+                return _checkFeatureLayerList;
+            }
+        }
+        private List<IFeatureLayer> _checkFeatureLayerList;
+
+        public List<string> CheckFeatureLayerNameList
+        {
+            get
+            {
+                return _checkFeatureLayerNameList;
+            }
+        }
+        private List<string> _checkFeatureLayerNameList;
+
+        private GApplication _app;
+        public string mdbPath;
+
+
+        public CheckFCFieldValueFrm(GApplication app)
+        {
+            InitializeComponent();
+            _app = app;
+        }
+
+        private void btOK_Click(object sender, EventArgs e)
+        {
+            _checkFeatureLayerList = new List<IFeatureLayer>();
+            _checkFeatureLayerNameList = new List<string>();
+
+            foreach (var lyr in chkLayerList.CheckedItems)
+            {
+                var kv = (KeyValuePair<IFeatureLayer, string>)lyr;
+                _checkFeatureLayerList.Add(kv.Key);
+                _checkFeatureLayerNameList.Add(kv.Value);
+            }
+
+            if (CheckFeatureLayerNameList.Count == 0)
+            {
+                return;
+            }
+
+            DialogResult = System.Windows.Forms.DialogResult.OK;
+        }
+
+        private void CheckFCFieldValueFrm_Load(object sender, EventArgs e)
+        {
+            // 在构造函数或 Load 事件中添加选项
+            mdbPath = _app.Template.Root + "\\质检\\属性逻辑关系硬编码检查.mdb";
+            List<string> tableNames = GetTableNamesFromMDB(mdbPath);
+
+            // 使用 foreach 循环遍历列表中的元素
+            chkLayerList.ValueMember = "Key";
+            chkLayerList.DisplayMember = "Value";
+
+            foreach (string tableName in tableNames)
+            {
+                IFeatureLayer tableNameLayer = (_app.Workspace.LayerManager.GetLayer(
+                    l => (l is IGeoFeatureLayer) && ((l as IGeoFeatureLayer).Name.ToUpper() == tableName)).FirstOrDefault() as IFeatureLayer);
+                chkLayerList.Items.Add(new KeyValuePair<IFeatureLayer, string>(tableNameLayer, tableName));
+            }
+        }
+
+        public static List<string> GetTableNamesFromMDB(string mdbFilePath)
+        {
+            List<string> tableNames = new List<string>();
+
+            string connString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + mdbFilePath + ";";
+            using (OleDbConnection conn = new OleDbConnection(connString))
+            {
+                conn.Open();
+
+                // 获取表格信息
+                var schema = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+
+                if (schema != null)
+                {
+                    foreach (DataRow row in schema.Rows)
+                    {
+                        string tableName = row["TABLE_NAME"].ToString();
+                        tableNames.Add(tableName);
+                    }
+                }
+            }
+
+            return tableNames;
+        }
+
+        private void btnSelAll_Click(object sender, EventArgs e)
+        {
+            for (var i = 0; i < chkLayerList.Items.Count; i++)
+            {
+                chkLayerList.SetItemChecked(i, true);
+            }
+        }
+
+        private void btnUnSelAll_Click(object sender, EventArgs e)
+        {
+            for (var i = 0; i < chkLayerList.Items.Count; i++)
+            {
+                chkLayerList.SetItemChecked(i, false);
+            }
+            chkLayerList.ClearSelected();
+        }
+    }
+}
---
+++ SMGI.Plugin.CollaborativeWorkWithAccount/开发内容/第十七周/要素分层硬编码逻辑检查/CheckFCFieldValueFrm.designer.cs
@@ -0,0 +1,153 @@
+﻿namespace SMGI.Plugin.CollaborativeWorkWithAccount
+{
+    partial class CheckFCFieldValueFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btOK = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkLayerList = new System.Windows.Forms.CheckedListBox();
+            this.btnSelAll = new System.Windows.Forms.Button();
+            this.btnUnSelAll = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btOK);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.btCancel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 241);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(4);
+            this.panel1.Size = new System.Drawing.Size(469, 31);
+            this.panel1.TabIndex = 9;
+            // 
+            // btOK
+            // 
+            this.btOK.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btOK.Location = new System.Drawing.Point(328, 4);
+            this.btOK.Name = "btOK";
+            this.btOK.Size = new System.Drawing.Size(64, 23);
+            this.btOK.TabIndex = 7;
+            this.btOK.Text = "确定";
+            this.btOK.UseVisualStyleBackColor = true;
+            this.btOK.Click += new System.EventHandler(this.btOK_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(392, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(9, 23);
+            this.panel2.TabIndex = 6;
+            // 
+            // btCancel
+            // 
+            this.btCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btCancel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btCancel.Location = new System.Drawing.Point(401, 4);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(64, 23);
+            this.btCancel.TabIndex = 5;
+            this.btCancel.Text = "取消";
+            this.btCancel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 12);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "选择需要检查的图层";
+            // 
+            // chkLayerList
+            // 
+            this.chkLayerList.FormattingEnabled = true;
+            this.chkLayerList.Location = new System.Drawing.Point(20, 30);
+            this.chkLayerList.Name = "chkLayerList";
+            this.chkLayerList.Size = new System.Drawing.Size(372, 196);
+            this.chkLayerList.TabIndex = 13;
+            // 
+            // btnSelAll
+            // 
+            this.btnSelAll.Location = new System.Drawing.Point(401, 30);
+            this.btnSelAll.Name = "btnSelAll";
+            this.btnSelAll.Size = new System.Drawing.Size(61, 23);
+            this.btnSelAll.TabIndex = 33;
+            this.btnSelAll.Text = "全选";
+            this.btnSelAll.UseVisualStyleBackColor = true;
+            this.btnSelAll.Click += new System.EventHandler(this.btnSelAll_Click);
+            // 
+            // btnUnSelAll
+            // 
+            this.btnUnSelAll.Location = new System.Drawing.Point(401, 59);
+            this.btnUnSelAll.Name = "btnUnSelAll";
+            this.btnUnSelAll.Size = new System.Drawing.Size(61, 23);
+            this.btnUnSelAll.TabIndex = 32;
+            this.btnUnSelAll.Text = "全部取消";
+            this.btnUnSelAll.UseVisualStyleBackColor = true;
+            this.btnUnSelAll.Click += new System.EventHandler(this.btnUnSelAll_Click);
+            // 
+            // CheckFCFieldValueFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 272);
+            this.Controls.Add(this.btnSelAll);
+            this.Controls.Add(this.btnUnSelAll);
+            this.Controls.Add(this.chkLayerList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CheckFCFieldValueFrm";
+            this.Text = "要素分层硬编码逻辑检查";
+            this.Load += new System.EventHandler(this.CheckFCFieldValueFrm_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btOK;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox chkLayerList;
+        private System.Windows.Forms.Button btnSelAll;
+        private System.Windows.Forms.Button btnUnSelAll;
+    }
+}
